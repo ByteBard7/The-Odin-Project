5e367daf19a68880f7e599738c85a16317e7947d
bytebard7 <128482646+bytebard7@users.noreply.github.com>
2024-02-27 19:42:48 +0530

    add wanna play again loop

--- RPS/rps.js
+++ RPS/rps.js
@@ -1,95 +1,112 @@
-
-let n = 0; // this variable is used in while loop to determine how many rounds to play
-
-// asking user input for how many rounds to play 
-const rounds = Number(prompt("How Many Rounds Do You Want To Play? : "));
-
-
-// computer and player score 
 let compScore = 0;
 let playerScore = 0;
 
-// checking the type of rounds variable
-if (typeof(rounds) !== 'number'){
-  console.log('Enter a Valid Value/Number');
-}
 
+function playGame() {
+  let n = 0; // this variable is used in while loop to determine how many rounds to play
 
-// round count indicator
-let roundCount = 0;
+  // asking user input for how many rounds to play
+  const rounds = Number(prompt("How Many Rounds Do You Want To Play? : "));
 
-// looping through rounds 
-while (n < rounds) {
-  n ++;
-  roundCount ++;
+  // computer and player score
 
-
-  // computer choices
-  const choices = ["rock", "paper", "scissors"];
-  let randomChoice = Math.floor(Math.random() * choices.length);
-
-  // selecting random choice for computer 
-  function getComputerChoice() {
-    return choices[randomChoice];
+  // checking the type of rounds variable
+  if (typeof rounds !== "number") {
+    console.log("Enter a Valid Value/Number");
   }
 
-  // asking user to input their choice 
-  const playerSelection = prompt(
-    "Enter Your Choice (Rock/Paper/Scissors): "
-  ).toLowerCase();
+  // round count indicator
+  let roundCount = 0;
 
-  // assigning computer choice to variable
-  const computerSelection = getComputerChoice()
+  // looping through rounds
+  while (n < rounds) {
+    n++;
+    roundCount++;
 
-  // comparing choices 
-  function playRound(computerSelection, playerSelection) {
-    if (computerSelection === playerSelection) {
-      return `Its a Tie,You both chose ${computerSelection}`;
-    } else if (computerSelection === "rock" && playerSelection === "scissors") {
-      compScore++;
-      return `You Lose! Rock beats Scissors.`;
-    } else if (computerSelection === "scissors" && playerSelection === "rock") {
-      playerScore++;
-      return `You Win! Rock beats Scissors.`;
-    } else if (computerSelection === "rock" && playerSelection === "paper") {
-      playerScore++;
-      return `You Win! Paper beats Rock.`;
-    } else if (computerSelection === "paper" && playerSelection === "rock") {
-      compScore++;
-      return `You Lose! Paper beats Rock.`;
-    } else if (
-      computerSelection === "paper" &&
-      playerSelection === "scissors"
-    ) {
-      playerScore++;
-      return `You Win! Scissors beats Paper.`;
-    } else if (
-      computerSelection === "scissors" &&
-      playerSelection === "paper"
-    ) {
-      compScore++;
-      return `You Lose! Scissors beats Paper.`;
-    } else {
-      return "Enter a correct choice between (Rock/Paper/Scissors)";
+    // computer choices
+    const choices = ["rock", "paper", "scissors"];
+    let randomChoice = Math.floor(Math.random() * choices.length);
+
+    // selecting random choice for computer
+    function getComputerChoice() {
+      return choices[randomChoice];
     }
+
+    // asking user to input their choice
+    const playerSelection = prompt(
+      "Enter Your Choice (Rock/Paper/Scissors): "
+    ).toLowerCase();
+
+    // assigning computer choice to variable
+    const computerSelection = getComputerChoice();
+
+    // comparing choices
+    function playRound(computerSelection, playerSelection) {
+      if (computerSelection === playerSelection) {
+        return `Its a Tie,You both chose ${computerSelection}`;
+      } else if (
+        computerSelection === "rock" &&
+        playerSelection === "scissors"
+      ) {
+        compScore++;
+        return `You Lose! Rock beats Scissors.`;
+      } else if (
+        computerSelection === "scissors" &&
+        playerSelection === "rock"
+      ) {
+        playerScore++;
+        return `You Win! Rock beats Scissors.`;
+      } else if (computerSelection === "rock" && playerSelection === "paper") {
+        playerScore++;
+        return `You Win! Paper beats Rock.`;
+      } else if (computerSelection === "paper" && playerSelection === "rock") {
+        compScore++;
+        return `You Lose! Paper beats Rock.`;
+      } else if (
+        computerSelection === "paper" &&
+        playerSelection === "scissors"
+      ) {
+        playerScore++;
+        return `You Win! Scissors beats Paper.`;
+      } else if (
+        computerSelection === "scissors" &&
+        playerSelection === "paper"
+      ) {
+        compScore++;
+        return `You Lose! Scissors beats Paper.`;
+      } else {
+        prompt("Enter a correct choice between (Rock/Paper/Scissors)");
+      }
+    }
+
+    // displaying details about each round
+    function game() {
+      console.log(`Round No.${roundCount}`);
+      console.log(`Computer Chose : ${computerSelection}`);
+      console.log(`Player Chose : ${playerSelection}`);
+      console.log(playRound(computerSelection, playerSelection));
+      showCompScore.innerText = `Computer Score : ${compScore}`;
+    }
+
+    game();
   }
 
-
-  // displaying details about each round 
-  function game() {
-    console.log(`Round No.${roundCount}`)
-    console.log(`Computer Chose : ${computerSelection}`);
-    console.log(`Player Chose : ${playerSelection}`);
-    console.log(playRound(computerSelection, playerSelection));
+  // displaying total score at the end
+  if (n === rounds) {
+    console.log(`Computer score is ${compScore}`);
+    console.log(`Your score is ${playerScore}`);
   }
-
-  game();
 }
 
+playGame();
 
+let wannaPlay = true;
 
-// displaying total score at the end 
-if (n === rounds) {
-  console.log(`Computer score is ${compScore}`);
-  console.log(`Your score is ${playerScore}`);
+while (wannaPlay !== false) {
+  let playAgain = prompt("Wanna play again ? (yes/no) : ").toLowerCase();
+  if (playAgain === "yes") {
+    playGame();
+  } else {
+    wannaPlay = false;
+  }
 }
